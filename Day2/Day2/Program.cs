d2d70f1056237f2529cffc99f8445b0d71b3e1aa
Jennifer Hallberg <77543556+Piixiiee@users.noreply.github.com>
2023-12-04 22:58:51 +0100

    Add files via upload

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+int maxRed = 12;
+int maxGreen = 13; 
+int maxBlue = 14;
+
+// testgames
+
+
+//string [] testGameFailure = {8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red}; 
+//string [] testGameSuccess = {3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green}; 
+
+string [] Game = {"1", "blue"}; 
+string [] Numbers = {"0","1","2","3","4","5","6","7","8","9",}
+//  Method for compariosns 
+foreach (string i in Game)
+{
+    if (i == Numbers)
+    {
+        Console.WriteLine(i); 
+    }
+}
+
+/*if (red)
+{
+
+}
+else if (green)
+{
+
+}
+else if (blue)
+{
+
+}
+*/ 
+// Splitting arays into elements 
